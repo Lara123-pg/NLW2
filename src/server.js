6aa64cd9aa295293417a30a33407b3e15b3687a6
lara123-pg <lamorimalvescavalcante@gmail.com>
2022-01-10 14:30:52 -0300

    NLW2

---
+++ src/server.js
@@ -0,0 +1,34 @@
+//Servidor
+
+const express=require('express')
+const server=express()
+
+const{
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+}=require('./pages')
+
+
+//Template Engine
+const nunjucks=require('nunjucks')
+nunjucks.configure('src/views',{
+    express:server,
+    noCache: true,
+})
+
+//Início e configuração do servidor
+
+server
+//Receber os dados do req.body
+.use(express.urlencoded({extended: true}))
+.use(express.static("public"))
+
+.get("/",pageLanding)
+.get("/study",pageStudy)
+.get("/give-classes",pageGiveClasses)
+.post("/save-classes",saveClasses)
+//Start do servidor
+.listen(5500)
+
